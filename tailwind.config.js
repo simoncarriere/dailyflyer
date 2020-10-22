9c5fb2c3949de4db400cc4079444dd7527cf0d28
simoncarriere <simoncarriere@icloud.com>
2020-10-22 16:36:46 +0545

    Barebones HTML and Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EFF2FA",
+        secondary: {
+          100: "#091233",
+          200: "#151F43",
+          300: "#151515",
+        },
+      },
+      fontFamily: {
+        body: "Commissioner",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
